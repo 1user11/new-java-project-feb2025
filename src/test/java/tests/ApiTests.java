4f13bf6b2fd9a8cd407518d2200ffe337534292d
ok <okepych@yahoo.com>
2025-02-17 01:45:53 -0600

    parameterized test added

--- src/test/java/tests/ApiTests.java
+++ src/test/java/tests/ApiTests.java
@@ -3,8 +3,13 @@ package tests;
 import controllers.UserController;
 import io.restassured.response.Response;
 import models.AddUserResponse;
+import models.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -88,8 +93,24 @@ class ApiTests {
     }
 
     @Test
-    void createUserControllerTest2() {
+    void createInvalidUserControllerTest() {
         Response response = userController.createUser(INVALID_USER);
+        AddUserResponse createdUserResponse = response.as(AddUserResponse.class);
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(200, createdUserResponse.getCode());
+        Assertions.assertEquals("unknown", createdUserResponse.getType());
+        Assertions.assertFalse(createdUserResponse.getMessage().isEmpty());
+    }
+
+    static Stream<User> users() {
+        return Stream.of(DEFAULT_USER, INVALID_USER);
+    }
+
+    @ParameterizedTest
+    @MethodSource("users")
+    void createUserParametrizedTest(User user) {
+        Response response = userController.createUser(user);
         AddUserResponse createdUserResponse = response.as(AddUserResponse.class);
 
         Assertions.assertEquals(200, response.statusCode());
